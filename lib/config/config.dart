bf734fad0851a1ac6745f1e7f7f113f7ded3f5b9
Moeez1999 <55261847+Moeez1999@users.noreply.github.com>
2022-02-21 01:07:48 +0500

    all done

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'Pak Torism Hub'; //app name
+  final String testerPassword = 'tester12345';  //testing password
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAAR_S7yC4:APA91bFPQ9lHQWU3KOFX12TsXcWsVxWyPQ10CX90fXMN8NLkW96G_reh3__u34EWh0eDjUCNZI3tDE3teAwEosW8twTQlauTkxiC5k6wOjxMma8Q6L1CGitiuXC1TKRJuI8wMRNcRBkz';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
